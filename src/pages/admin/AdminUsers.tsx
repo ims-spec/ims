697d7ae736c22d413b0f286871e2e0cad3fcc0be
Farhod <uzkorims@gmail.com>
2025-03-26 22:36:31 +0500

    test AdminUsers page

--- src/pages/admin/AdminUsers.tsx
+++ src/pages/admin/AdminUsers.tsx
@@ -1,19 +1,33 @@
 import { useEffect } from "react";
-import { useAuth } from "../../store/useAuth";
+import { useStore } from "../../store/useStore";
+
 
 
 export const AdminUsers = () => {
+    const getAuthors = useStore((state) => state.getAuthors)
+    const authors = useStore((state) => state.authors)
+    const error = useStore((state) => state.error)
+    const loading = useStore((state) => state.loading)
 
-    const {getAllUsers} = useAuth()
+
 
     useEffect(() => {
-      getAllUsers()  
-    },[getAllUsers])   
+        getAuthors().then()
+    }, [getAuthors])
+
+    if (loading) {
+        return <p>Loading ...</p>
+    }
+
 
     return (
         <div className="container">
             <h1>Admin users</h1>
-
+            <h5>Authors:</h5>
+            {error && <p>{error}</p>}
+            {authors.map((author) => (
+                <li key={author.id}>{author.last_name}</li>
+            ))}
         </div>
     );
 };
